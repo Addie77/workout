6ecdf27606fede3fa50decb38d93d06c3e383e85
addie <addie931124@gmail.com>
2025-12-12 22:51:18 -0800

    text

---
+++ workout/WorkoutPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutPlayerView.swift
+//  workout
+//
+//  Created by user0862 on 2025/12/12.
+//
+
+import SwiftUI
+
+struct WorkoutPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutPlayerView()
+}
